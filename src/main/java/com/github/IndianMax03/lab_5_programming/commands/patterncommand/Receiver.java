1aa4f2760e8471f9023c8ad6b871016a2eeffbd6
IndianMax <vermamanu@mail.ru>
2022-04-06 22:08:48 +0300

    a few command(-s)

--- src/main/java/com/github/IndianMax03/lab_5_programming/commands/patterncommand/Receiver.java
+++ src/main/java/com/github/IndianMax03/lab_5_programming/commands/patterncommand/Receiver.java
@@ -1,37 +1,18 @@
 package com.github.indianMax03.lab_5_programming.commands.patterncommand;
 
 import com.github.indianMax03.lab_5_programming.base.City;
-import com.github.indianMax03.lab_5_programming.commands.Add;
+import com.github.indianMax03.lab_5_programming.base.Government;
 import com.github.indianMax03.lab_5_programming.commands.addition.AddCity;
+import com.github.indianMax03.lab_5_programming.commands.addition.HelpCalling;
 
+import java.util.Iterator;
 import java.util.TreeSet;
 
 public class Receiver {
 
     public void help(){
-        System.out.println("Введите help, если хотитите вывести справку по доступным командам");
-        System.out.println("Введите info, чтобы вывести в стандартный поток вывода информацию о коллекции (тип, дата" +
-                " инициализации, количество элементов и т.д.)");
-        System.out.println("Введите show, чтобы вывести в стандартный поток вывода все элементы коллекции в строковом" +
-                " представлении");
-        System.out.println("Введите add {element}, чтобы добавить новый элемент в коллекцию");
-        System.out.println("Введите update id {element}, чтобы обновить значение элемента коллекции, id которого равен заданному");
-        System.out.println("Введите remove_by_id id, чтобы удалить элемент из коллекции по его id");
-        System.out.println("Введите clear, чтобы очистить коллекцию");
-        System.out.println("Введите save, чтобы сохранить коллекцию в файл");
-        System.out.println("Введите execute_script file_name, чтобы считать и исполнить скрипт из указанного файла. " +
-                "В скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме.");
-        System.out.println("Введите exit, чтобы завершить программу (без сохранения в файл)");
-        System.out.println("Введите add_if_min {element}, чтобы добавить новый элемент в коллекцию, если его значение" +
-                " меньше, чем у наименьшего элемента этой коллекции");
-        System.out.println("Введите remove_greater {element}, чтобы удалить из коллекции все элементы, превышающие заданный");
-        System.out.println("Введите remove_lower {element}, чтобы удалить из коллекции все элементы, меньшие, чем заданный");
-        System.out.println("Введите remove_all_by_government government, чтобы удалить из коллекции все элементы, " +
-                "значение поля government которого эквивалентно заданному");
-        System.out.println("Введите filter_starts_with_name name, чтобы вывести элементы, значение поля name которых " +
-                "начинается с заданной подстроки");
-        System.out.println("Введите print_descending, чтобы вывести элементы коллекции в порядке убывания");
-    }
+        new HelpCalling();
+        }
 
     public void exit(){
         System.out.println("Спасибо за работу, до свидания!");
@@ -73,6 +54,24 @@ public class Receiver {
                 System.out.println(city);
             }
         }
+    }
+
+    public void printDescending(TreeSet<City> collection){
+        System.out.println("Элементы коллекции в порядке убывания (обратном): ");
+        if (collection.isEmpty()){
+            System.out.println("Коллекция пуста.");
+        } else {
+            Iterator<City> i = collection.descendingIterator();
+            while (i.hasNext()){
+                System.out.println(i.next());
+            }
+        }
+    }
+
+    public void removeAllByGovernment(TreeSet<City> collection){
+        System.out.println("Из коллекции будут удалены все элементы, значение поля government которых соответствует данному.");
+        Government government = Government.chooseGovernment();
+        collection.removeIf(city -> city.getGovernment().equals(government));
     }
 
 }
