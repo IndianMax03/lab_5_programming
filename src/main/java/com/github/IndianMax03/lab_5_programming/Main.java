b69facee1362bf8893b5e02f7e0e68970a6c7a27
IndianMax <vermamanu@mail.ru>
2022-04-09 22:54:06 +0300

    NEW ARCHITECTURE

--- src/main/java/com/github/IndianMax03/lab_5_programming/Main.java
+++ src/main/java/com/github/IndianMax03/lab_5_programming/Main.java
@@ -2,12 +2,8 @@ package com.github.indianMax03.lab_5_programming;
 // helios ssh sXXXXXX@helios.se.ifmo.ru -p 2222
 import com.github.indianMax03.lab_5_programming.base.City;
 import com.github.indianMax03.lab_5_programming.commands.*;
-import com.github.indianMax03.lab_5_programming.commands.patterncommand.Command;
-import com.github.indianMax03.lab_5_programming.commands.patterncommand.CommandWithArgument;
-import com.github.indianMax03.lab_5_programming.commands.patterncommand.Invoker;
-import com.github.indianMax03.lab_5_programming.commands.patterncommand.Receiver;
+import com.github.indianMax03.lab_5_programming.commands.patterncommand.*;
 
-import java.time.ZonedDateTime;
 import java.util.*;
 
 
@@ -16,58 +12,61 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // Argument means is where we are reading
         Invoker invoker = new Invoker(); //  Хранилище команд
-        TreeSet<City> collection = new TreeSet<City>(); //  Main collection
+        TreeSet<City> collection = new TreeSet<>(); //  Main collection
         Receiver receiver = new Receiver(); //  Коробка с командами
 
-        Command help = new Help(receiver);
-        invoker.register("help", help);
+        Command add = new Add(receiver);
+        invoker.register("add", add);
+        Command addIfMin = new AddIfMin(receiver);
+        invoker.register("add_if_min", addIfMin);
         Command exit = new Exit(receiver);
         invoker.register("exit", exit);
-        Command clear = receiver::clear;
+        Command clear = new Clear(receiver);
         invoker.register("clear", clear);
-        Command add = receiver::add;
-        invoker.register("add", add);
-        Command show = receiver::show;
+        Command show = new Show(receiver);
         invoker.register("show", show);
-        Command addIfMin = receiver::addIfMin;
-        invoker.register("add_if_min", addIfMin);
-        CommandWithArgument filterStartsWithName = receiver::filterStartsWithName;
-        invoker.registerWithArg("filter_starts_with_name", filterStartsWithName);
-        Command printDescending = receiver::printDescending;
+        Command filterStartsWithName = new FilterStartsWithName(receiver);
+        invoker.register("filter_starts_with_name", filterStartsWithName);
+        Command printDescending = new PrintDescending(receiver);
         invoker.register("print_descending", printDescending);
-        Command removeAllByGovernment = receiver::removeAllByGovernment;
+        Command removeAllByGovernment = new RemoveAllByGovernment(receiver);
         invoker.register("remove_all_by_government", removeAllByGovernment);
-        CommandWithArgument removeById = receiver::removeById;
-        invoker.registerWithArg("remove_by_id", removeById);
-        Command removeGreater = receiver::removeGreater;
+        Command removeById = new RemoveByID(receiver);
+        invoker.register("remove_by_id", removeById);
+        Command removeGreater = new RemoveGreater(receiver);
         invoker.register("remove_greater", removeGreater);
-        Command removeLower = receiver::removeLower;
+        Command removeLower = new RemoveLower(receiver);
         invoker.register("remove_greater", removeLower);
-        Command info = receiver::info;
+        Command info = new Info(receiver);
         invoker.register("info", info);
+        Command updateId = new UpdateId(receiver);
+        invoker.register("update", updateId);
+        Command help = new Help(receiver);
+        invoker.register("help", help);
+
 
 
         System.out.println("Для вывода справки по командам введите help.");
+        String out = "";
         while (true) {
             System.out.print("Введите команду:\n>");
             String[] commandline = sc.nextLine().trim().split(" ");
-            if (commandline.length == 1) {
-                String command = commandline[0];
-                try {
-                    invoker.execute(command, collection);
-                } catch (Exception e) {
-                    System.out.println("Команда не найдена. Повторите ввод.");
+            String command = commandline[0];
+            if (command.isEmpty()){
+                continue;
+            }
+            if (commandline.length == 1){
+                out = invoker.execute(invoker, command, collection, "");
+                if (out == null){
+                    break;
                 }
+                System.out.println(out);
             } else if (commandline.length == 2){
-                String command  = commandline[0];
-                String argument = commandline[1];
-                try {
-                    invoker.executeWithArg(command, collection, argument);
-                } catch (Exception e){
-                    System.out.println("Команда не найдена. Повторите ввод.");
-                }
+                out = invoker.execute(invoker, command,collection, commandline[1]);
+                System.out.println(out);
             } else {
-                System.out.println("Команда не найдена. Повторите ввод.");
+                out = "Команды не существует";
+                System.out.println(out);
             }
         }
     }
--- src/main/java/com/github/IndianMax03/lab_5_programming/commands/UpdateID.java
+++ src/main/java/com/github/IndianMax03/lab_5_programming/commands/UpdateID.java
@@ -1,4 +1,32 @@
 package com.github.indianMax03.lab_5_programming.commands;
 
-public class UpdateID {
+import com.github.indianMax03.lab_5_programming.base.City;
+import com.github.indianMax03.lab_5_programming.commands.patterncommand.Command;
+import com.github.indianMax03.lab_5_programming.commands.patterncommand.Invoker;
+import com.github.indianMax03.lab_5_programming.commands.patterncommand.Receiver;
+
+import java.util.TreeSet;
+
+public class UpdateId implements Command {
+
+
+    private final Receiver receiver;
+
+    public UpdateId(Receiver receiver){
+        this.receiver = receiver;
+    }
+
+    @Override
+    public String execute(Invoker invoker, TreeSet<City> collection, String argument) {
+        if (!argument.isEmpty()) {
+            return receiver.updateId(collection, argument);
+        } else {
+            return "Команде update_id требуется аргумент id";
+        }
+    }
+
+    @Override
+    public String getHelp(){
+        return "Введите update id {element}, чтобы обновить значение элемента коллекции, id которого равен заданному";
+    }
 }
